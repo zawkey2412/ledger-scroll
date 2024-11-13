2fd2e82bd0a5820e00db553351fd9482f36f4c7a
zawkey2412 <24.zawkey@gmail.com>
2024-11-13 20:15:57 +0700

    final-check:build

--- src/components/route/publicRoute.tsx
+++ src/components/route/publicRoute.tsx
@@ -1,10 +1,20 @@
 import { Navigate } from "react-router-dom";
 import useAuthStore from "../../store/useAuthStore";
+import { useEffect, useState } from "react";
 
 const PublicRoute = ({ children }: { children: JSX.Element }) => {
   const { user, isEmailVerified, loading } = useAuthStore();
+  const [showSpinner, setShowSpinner] = useState(false);
 
-  if (loading) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSpinner(loading);
+    }, 300); // 300ms delay
+
+    return () => clearTimeout(timer);
+  }, [loading]);
+
+  if (showSpinner) {
     return <div className="loading-spinner"></div>;
   }
 
